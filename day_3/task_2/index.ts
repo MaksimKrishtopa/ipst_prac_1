1b6e2df90f4728e002656aa3e3e238de3d53db0a
Maks_Krishtopa <k.maks21@mail.ru>
2025-01-15 20:38:18 +0700

    мелкие исправления

--- day_3/task_2/index.ts
+++ day_3/task_2/index.ts
@@ -1,15 +1,16 @@
-const fetchData = async (url: string): Promise<any> => {
+const fetchData = async (url : string) : Promise <any> => {
     const response = await fetch(url)
-    return response.json()
-  };
-  
-  (async () => {
-    const apiUrl = "https://reqres.in/api/users/";
+    return response.json();
+};
+
+(async () => {
+    const apiUrl = "https://reqres.in/api/users/"
     try {
-      const result = await fetchData(apiUrl)
-      console.log(result)
-    } catch (error) {
-      console.error("Ошибка при загрузке данных", error instanceof Error ? error.message : error)
+        const result = await fetchData(apiUrl)
+        console.log(result)
     }
-  })();
-  
+    catch (error) {
+        console.error ("Ошибка", error instanceof Error ? error.message : error)
+    }
+} 
+)()
